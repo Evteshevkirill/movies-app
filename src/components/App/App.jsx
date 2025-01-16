8b395c74595f126dfe030cb044204123a11a4963
Evteshev Kirill <ergoboxvn@gmail.com>
2025-01-16 12:41:34 +0300

    first

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,14 @@
+import { Component } from 'react'
+
 import './App.css'
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
